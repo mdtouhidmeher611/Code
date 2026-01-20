9f4615caac4f1354432e09ce3058edfc7fd8e096
MD. TOUHID MEHER (2410276120) <mdtouhidmeher@gmail.com>
2026-01-20 22:23:24 +0600

    20-1-2026

---
+++ 58B.py
@@ -0,0 +1,13 @@
+n=int(input())
+res=[n]
+
+while n>1:
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            n//=i
+            break
+    else:
+        n=1
+    res.append(n)
+
+print(*res)
